857df97e511729bde6fa8857f45ffe84b71c5d69
CR Hapuarachchi <112224823+0019-KDU@users.noreply.github.com>
2024-07-03 22:04:15 +0530

    create a login function
    issue the jwt token for login function

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,9 @@
 import prisma from "../DB/db.config.js";
 import vine, { errors } from "@vinejs/vine";
-import { registerSchema } from "../validation/authValidation.js";
+import { registerSchema, loginSchema } from "../validation/authValidation.js";
 import bcrypt from "bcryptjs";
 import { messages } from "@vinejs/vine/defaults";
+import jwt from "jsonwebtoken";
 
 class AuthController {
   static async register(req, res) {
@@ -45,7 +46,61 @@ class AuthController {
     }
   }
 
-  static async login(req, res) {}
+  static async login(req, res) {
+    try {
+      const body = req.body;
+      const validator = vine.compile(loginSchema);
+      const payload = await validator.validate(body);
+
+      //* find user with email
+      const findUser = await prisma.users.findUnique({
+        where: { email: payload.email },
+      });
+
+      if (findUser) {
+        if (!bcrypt.compareSync(payload.password, findUser.password)) {
+          return res.status(400).json({
+            errors: {
+              password: "Invalid Credentials",
+            },
+          });
+        }
+
+        //* Issue token to user
+        const payloadData = {
+          id: findUser.id,
+          name: findUser.name,
+          email: findUser.email,
+          profile: findUser.profile,
+        };
+
+        const token = jwt.sign(payloadData, process.env.JWT_SECRET, {
+          expiresIn: "365d",
+        });
+
+        return res.json({
+          messages: "Logged in",
+          access_token: `Bearer ${token}`,
+        });
+      }
+
+      return res.status(400).json({
+        errors: {
+          email: "No User Found with this email address",
+        },
+      });
+    } catch (error) {
+      console.log("The Error is: " + error);
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        // console.log(error.messages);'
+        return res.status(400).json({ errors: error.messages });
+      } else {
+        return res
+          .status(500)
+          .json({ status: 500, message: "Something went wrong.pls try again" });
+      }
+    }
+  }
 }
 
 export default AuthController;
