b2169272e19458623b5c291aa4234eb989c832de
CR Hapuarachchi <112224823+0019-KDU@users.noreply.github.com>
2024-07-07 22:02:58 +0530

    Pagination in News API

--- controllers/NewsController.js
+++ controllers/NewsController.js
@@ -6,7 +6,22 @@ import vine, { errors } from "@vinejs/vine";
 
 class NewsController {
   static async index(req, res) {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 1;
+
+    if (page <= 0) {
+      page = 1;
+    }
+
+    if (limit <= 0 || limit > 100) {
+      limit = 10;
+    }
+
+    const skip = (page - 1) * limit;
+
     const news = await prisma.news.findMany({
+      take: limit,
+      skip: skip,
       include: {
         user: {
           select: {
@@ -18,7 +33,18 @@ class NewsController {
       },
     });
     const newsTransform = news?.map((item) => NewsApiTransform.transform(item));
-    return res.json({ status: 200, news: newsTransform });
+
+    const totalNews = await prisma.news.count();
+    const totalPages = Math.ceil(totalNews / limit);
+    return res.json({
+      status: 200,
+      news: newsTransform,
+      metadata: {
+        totalPages,
+        currentPage: page,
+        currentLimit: limit,
+      },
+    });
   }
 
   static async store(req, res) {
